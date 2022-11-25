d5c8b0d578f27b437807675fb2e18e1efae30c34
Trusha1234 <trushagajerA1@gmail.com>
2022-11-25 11:11:14 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 
 import 'package:currency_con/second_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'first_page.dart';
 
-void main()
-{
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    // options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(home: first(),));
-
 }
